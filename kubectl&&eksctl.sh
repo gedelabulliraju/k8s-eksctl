4be2c66e4b5d743e0985656823a7ee29697001a9
Bulliraju <bulliraju118@gmail.com>
2025-08-12 17:24:09 +0530

    updated installations

---
+++ kubectl&&eksctl.sh
@@ -0,0 +1,107 @@
+# This script installs the kubectl and eksctl tools for managing Kubernetes clusters on AWS EKS.
+#!/bin/bash
+
+# Install kubectl
+echo "Installing kubectl..."
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+if [ $? -ne 0 ]; then
+    echo "Failed to download kubectl. Please check your internet connection or the URL."
+    exit 1
+fi
+echo "Verifying kubectl checksum..."
+openssl sha1 -sha256 kubectl
+if [ $? -ne 0 ]; then
+    echo "Failed to verify kubectl checksum. Please check the file integrity."
+    exit 1
+fi
+echo "Making kubectl executable..."
+chmod +x ./kubectl
+if [ $? -ne 0 ]; then
+    echo "Failed to make kubectl executable. Please check permissions."
+    exit 1
+fi
+echo "Moving kubectl to /usr/local/bin..."
+sudo mv ./kubectl /usr/local/bin
+if [ $? -ne 0 ]; then
+    echo "Failed to move kubectl to /usr/local/bin. Please check permissions."
+    exit 1
+fi
+echo "kubectl installed successfully."
+kubectl version
+
+# Install eksctl
+echo "Installing eksctl..."
+
+ARCH=amd64
+
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+if [ $? -ne 0 ]; then
+    echo "Failed to download or extract eksctl. Please check your internet connection or the URL."
+    exit 1
+fi
+echo "Moving eksctl to /usr/local/bin..."
+sudo mv /tmp/eksctl /usr/local/bin
+if [ $? -ne 0 ]; then
+    echo "Failed to move eksctl to /usr/local/bin. Please check permissions."
+    exit 1
+fi
+echo "eksctl installed successfully."
+eksctl version
+echo "Installation of kubectl and eksctl completed successfully."
+
+# Install Matrix server for Horizontal Pod Autoscaler in k8s
+echo "Installing Horizontal Pod Autoscaler..."
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+if [ $? -ne 0 ]; then
+    echo "Failed to install Horizontal Pod Autoscaler. Please check your internet connection or the URL."
+    exit 1
+fi
+echo "Horizontal Pod Autoscaler installed successfully."
+
+# Install k9s
+echo "Installing k9s..."
+curl -sS https://webinstall.dev/k9s | bash
+if [ $? -ne 0 ]; then
+    echo "Failed to install k9s. Please check your internet connection or the URL."
+    exit 1
+fi
+echo "k9s installed successfully."
+
+# Install Helm
+echo "Installing Helm..."
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+if [ $? -ne 0 ]; then
+    echo "Failed to install Helm. Please check your internet connection or the URL."
+    exit 1
+fi
+echo "Helm installed successfully."
+
+# Install ebs-csi-driver
+echo "Installing EBS CSI Driver..."
+helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
+helm repo update
+helm install aws-ebs-csi-driver aws-ebs-csi-driver/aws-ebs-csi-driver --namespace kube-system
+if [ $? -ne 0 ]; then
+    echo "Failed to install EBS CSI Driver. Please check your internet connection or the Helm repository."
+    exit 1
+fi
+echo "EBS CSI Driver installed successfully."
+
+# verify installations
+echo "Verifying installations..."
+kubectl version --short
+eksctl version
+k9s version
+helm version
+kubectl get pods -n kube-system
+if [ $? -ne 0 ]; then
+    echo "Failed to verify Kubernetes cluster. Please check your kubectl configuration."
+    exit 1
+fi
+echo "All installations verified successfully."
+
---
+++ resize&&docker-t3micro.sh
@@ -0,0 +1,123 @@
+# Description: This script resizes the disk partitions and filesystems on a Linux system.
+# Usage: Run this script with root privileges to resize the partitions and filesystems.     
+#!/bin/bash
+lsblk
+# Resize the partitions and filesystems
+sudo growpart /dev/nvme0n1 4
+if [ $? -ne 0 ]
+then
+    echo "Failed to resize the partition, please check the logs."
+    exit 1
+else
+    echo "Partition resized successfully."
+fi
+# Extend the logical volumes and resize the filesystems
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+if [ $? -ne 0 ]
+then
+    echo "Failed to extend the root logical volume, please check the logs."
+    exit 1
+else
+    echo "Root logical volume extended successfully."
+fi
+
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+if [ $? -ne 0 ]
+then
+    echo "Failed to extend the var logical volume, please check the logs."
+    exit 1
+else
+    echo "Var logical volume extended successfully."
+fi
+sudo xfs_growfs /
+if [ $? -ne 0 ]
+then
+    echo "Failed to resize the root filesystem, please check the logs."
+    exit 1
+else
+    echo "Root filesystem resized successfully."
+fi
+sudo xfs_growfs /var
+if [ $? -ne 0 ]
+then
+    echo "Failed to resize the var filesystem, please check the logs."
+    exit 1
+else
+    echo "Var filesystem resized successfully."
+fi
+
+#!/bin/bash
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root privileges"
+    exit 1
+fi
+#Add docker reposirory to config-manager 
+dnf -y install dnf-plugins-core
+if [ $? -ne 0 ]
+then
+    echo "Failed to install dnf-plugins-core, please check the logs."
+    exit 1
+else
+    echo "dnf-plugins-core installed successfully."
+fi
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+if [ $? -ne 0 ]
+then
+    echo "Failed to add Docker repository, please check the logs."
+    exit 1
+else
+    echo "Docker repository added successfully."
+fi
+#Install docker dependencies
+#Install using the rpm repository
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+if [ $? -ne 0 ]
+then
+    echo "Docker installation failed, please check the logs."
+    exit 1
+else
+    echo "Docker installation is successful."
+fi
+#add ec2-user to docker group
+usermod -aG docker ec2-user
+if [ $? -ne 0 ]
+then
+    echo "Failed to add user to docker group, please check the logs."
+    exit 1
+else
+    echo "User added to docker group successfully."
+fi
+#Start docker service
+systemctl start docker
+if [ $? -ne 0 ]
+then
+    echo "Failed to start docker service, please check the logs."
+    exit 1
+else
+    echo "Docker service started successfully."
+fi
+#Enable docker service to start on boot
+systemctl enable docker
+if [ $? -ne 0 ]
+then
+    echo "Failed to enable docker service on boot, please check the logs."
+    exit 1
+else
+    echo "Docker service enabled to start on boot successfully."
+fi
+#Check docker version
+docker --version
+if [ $? -ne 0 ]             
+then
+    echo "Docker is not installed correctly, please check the logs."
+    exit 1
+else
+    echo "Docker is installed and running successfully."
+fi
+echo "Docker installation and configuration completed successfully."
+echo "You can now use Docker commands without sudo."
+echo "Please log out and log back in to apply the group changes."
+echo "Thank you for using this script." 
